4166aa5e084fe1c23ca6d9f2617087ec0523a05c
monapu <mona@mona>
2015-09-06 21:46:03 +0900

    Testnet用の修正

--- core/src/main/java/com/google/bitcoin/core/Address.java
+++ core/src/main/java/com/google/bitcoin/core/Address.java
@@ -124,7 +124,7 @@ public class Address extends VersionedChecksummedBytes {
     @Nullable
     public NetworkParameters getParameters() {
         // TODO: There should be a more generic way to get all supported networks.
-        NetworkParameters[] networks = { MainNetParams.get() };
+        NetworkParameters[] networks = { MainNetParams.get() , TestNet3Params.get()};
         for (NetworkParameters params : networks) {
             if (isAcceptableVersion(params, version)) {
                 return params;
